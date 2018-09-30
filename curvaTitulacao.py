3b05d643778820330b2e6eefe9e58fcdc7290400
AlexEnrique <AlexEnrique@github.com>
2018-09-30 10:29:33 -0300

    Pratica 2 - Bioquimica
    Interpolação via Lagrange não dá bons resultados. De fato, a propria referência do SciPy dizia para não esperar bons resultados após uns 20 pontos.
    Tentei utilizar CubicSpline, porém o resultado é uma curva por partes, o que não fornece um resultado suave.
    
    Alternativas que estou pensando:
     - Suavizar via Bézier
     - Suavizar via Fourier
     - Definir uma função por partes com cada parte sendo dada por uma função logística e "fittar"
    
    <h3> Observações </h3>
    Existe um ponto que está incerto nos dados anotados. Vou perguntar no grupo a respeito, caso contrário ignoramos. A anotação que me deixou em dúvida ocorre para V = 16 mL.

---
+++ curvaTitulacao.py
@@ -0,0 +1,34 @@
+from scipy.interpolate import lagrange, CubicSpline
+from numpy import array
+from numpy.polynomial.polynomial import Polynomial
+import matplotlib.pyplot as plt
+
+pH = array([1.75, 2.32, 2.37, 2.44, 2.52, 2.62,
+            2.71, 2.80, 2.88, 2.98, 3.13, 3.29,
+            3.45, 3.78, 4.25, 5.55, #1.64, quando V = 16mL
+            8.80, 9.25, 9.45, 9.60, 9.76, 9.86,
+            9.95, 10.04, 10.17, 10.26, 10.35,
+            10.44, 10.58, 10.74, 10.90, 11.08,
+            11.27, 11.48, 11.65, 11.73, 11.81,
+            11.87, 11.91, 11.97, 12.03, 12.05,
+            12.08, 12.11, 12.14, 12.16, 12.17,
+            12.18, 12.19, 12.19
+           ])
+
+concentracao = array([0.125/1000 * n for n in range(0, 16)] + [0.125/1000 * n for n in range(17, 51)])
+# print(len(pH), len(concentracao))
+
+
+poly = lagrange(concentracao, pH)
+coefficients = poly.coef
+n = poly.order
+CS = CubicSpline(concentracao, pH)
+CSp = CS.derivative(1)
+CSpp = CS.derivative(2)
+
+plt.plot(concentracao, pH)
+# plt.plot(concentracao, CS(concentracao))
+# plt.plot(concentracao, CSp(concentracao))
+# plt.plot(concentracao, CSpp(concentracao))
+
+plt.show()
